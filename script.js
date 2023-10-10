010905b5f39ccb33a27ee9c2fa069ede452b1aa9
Guus Kaashoek <Guus.kaashoek@gmail.com>
2023-10-10 17:32:02 +0200

    olala
    pogies

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ function openMedia(element) {
     const media = element.querySelector('img, source');
     if (media) {
       const src = media.getAttribute('src');
-      window.open(src, '_blank');
+      window.open(src);
     }
+  }
+
+  function playVideo() {
+    var video = document.getElementById('mijnVideo');
+    video.play();
   }
