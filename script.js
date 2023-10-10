f3eaad6a22aad892e3456918d291266a143d51e4
Guus Kaashoek <Guus.kaashoek@gmail.com>
2023-10-10 10:29:56 +0200

    space

---
+++ script.js
@@ -0,0 +1,7 @@
+function openMedia(element) {
+    const media = element.querySelector('img, source');
+    if (media) {
+      const src = media.getAttribute('src');
+      window.open(src, '_blank');
+    }
+  }
