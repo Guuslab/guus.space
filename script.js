217d9849ed608a6e1a534abaee7e65e6e5c85eed
Guus Kaashoek <Guus.kaashoek@gmail.com>
2024-01-15 14:13:53 +0100

    Update styles and navigation

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
-function openMedia(element) {
-    const media = element.querySelector('img, source');
-    if (media) {
-      const src = media.getAttribute('src');
-      window.open(src);
-    }
-  }
+const hamburgerIcon = document.querySelector('.hamburger-icon-mob');
+const menuItems = document.querySelector('.menu-items-mob');
 
+hamburgerIcon.addEventListener('click', () => {
+  hamburgerIcon.classList.toggle('active');
+  menuItems.classList.toggle('active');
+});
+
+
+if (/Mobi/.test(navigator.userAgent) && !/iPad/.test(navigator.userAgent)) {
+
+  const menuElement = document.querySelector('.menu-items');
+  const iconElement = document.querySelector('.hamburger-icon');
+  const logoElement = document.querySelector('.logo');
+  const  skyContentElement = document.querySelector('.sky-content');
+  const  artxContentElement = document.querySelector('.artx-content');
+
+  menuElement.classList.remove('menu-items');
+  menuElement.classList.add('menu-items-mob');
+
+  iconElement.classList.remove('hamburger-icon');
+  iconElement.classList.add('hamburger-icon-mob');
+
+  logoElement.classList.remove('logo');
+  logoElement.classList.add('logo-mob');
+  
+  skyContentElement.classList.remove('sky-content');
+  skyContentElement.classList.add('sky-content-mob');
+
+  artxContentElement.classList.remove('artx-content');
+  artxContentElement.classList.add('artx-content-mob');
+}
