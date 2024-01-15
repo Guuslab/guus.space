da301590dfced23a7b653c3c6006f9c013541543
Guus Kaashoek <Guus.kaashoek@gmail.com>
2024-01-15 17:20:15 +0100

    Fix mobile navigation styles and add footer

--- script.js
+++ script.js
@@ -1,32 +1,82 @@
-const hamburgerIcon = document.querySelector('.hamburger-icon-mob');
-const menuItems = document.querySelector('.menu-items-mob');
+//based on an Example by @curran
+window.requestAnimFrame = (function(){   return  window.requestAnimationFrame})();
+var canvas = document.getElementById("space");
+var c = canvas.getContext("2d");
 
-hamburgerIcon.addEventListener('click', () => {
-  hamburgerIcon.classList.toggle('active');
-  menuItems.classList.toggle('active');
-});
+var numStars = 750;
+var radius = '0.'+Math.floor(Math.random() * 9) + 1  ;
+var focalLength = canvas.width *0.3;
+// var warp = 0;
+var centerX, centerY;
 
+var stars = [], star;
+var i;
 
-if (/Mobi/.test(navigator.userAgent) && !/iPad/.test(navigator.userAgent)) {
+var animate = true;
 
-  const menuElement = document.querySelector('.menu-items');
-  const iconElement = document.querySelector('.hamburger-icon');
-  const logoElement = document.querySelector('.logo');
-  const  skyContentElement = document.querySelector('.sky-content');
-  const  artxContentElement = document.querySelector('.artx-content');
+initializeStars();
 
-  menuElement.classList.remove('menu-items');
-  menuElement.classList.add('menu-items-mob');
-
-  iconElement.classList.remove('hamburger-icon');
-  iconElement.classList.add('hamburger-icon-mob');
-
-  logoElement.classList.remove('logo');
-  logoElement.classList.add('logo-mob');
+function executeFrame(){
   
-  skyContentElement.classList.remove('sky-content');
-  skyContentElement.classList.add('sky-content-mob');
-
-  artxContentElement.classList.remove('artx-content');
-  artxContentElement.classList.add('artx-content-mob');
+  if(animate)
+    requestAnimFrame(executeFrame);
+  moveStars();
+  drawStars();
 }
+
+function initializeStars(){
+  centerX = canvas.width / 2;
+  centerY = canvas.height / 2;
+  
+  stars = [];
+  for(i = 0; i < numStars; i++){
+    star = {
+      x: Math.random() * canvas.width,
+      y: Math.random() * canvas.height,
+      z: Math.random() * canvas.width,
+      o: '0.'+Math.floor(Math.random() * 99) + 1
+    };
+    stars.push(star);
+  }
+}
+
+function moveStars(){
+  for(i = 0; i < numStars; i++){
+    star = stars[i];
+    star.z--;
+    
+    if(star.z <= 0){
+      star.z = canvas.width;
+    }
+  }
+}
+
+function drawStars(){
+  var pixelX, pixelY, pixelRadius;
+  
+  // Resize to the screen
+  if(canvas.width != window.innerWidth || canvas.width != window.innerWidth){
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    initializeStars();
+  }
+//   if(warp==0)
+  {c.fillStyle = "rgba(0,0,0,1)";
+  c.fillRect(0,0, canvas.width, canvas.height);}
+  c.fillStyle = "rgba(255, 255, 255, "+radius+")";
+  for(i = 0; i < numStars; i++){
+    star = stars[i];
+    
+    pixelX = (star.x - centerX) * (focalLength / star.z);
+    pixelX += centerX;
+    pixelY = (star.y - centerY) * (focalLength / star.z);
+    pixelY += centerY;
+    pixelRadius = 1.5 * (focalLength / star.z);
+    
+    c.fillRect(pixelX, pixelY, pixelRadius, pixelRadius);
+    c.fillStyle = "rgba(255, 255, 255, "+star.o+")";
+    c.fill();
+  }
+}
+
+executeFrame();
