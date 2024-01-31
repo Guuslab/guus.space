3aaa25e9b21c9ae6ad2418350b098363af862ab1
Guus Kaashoek <Guus.kaashoek@gmail.com>
2024-01-31 13:31:31 +0100

    Add Google Fonts preconnect and stylesheet

--- pages/_document.js
+++ pages/_document.js
@@ -12,6 +12,9 @@ class MyDocument extends Document {
         <Head>
           {/* <title>guus.space</title> */}
           <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon"/>
+          <link rel="preconnect" href="https://fonts.googleapis.com"/>
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+          <link href="https://fonts.googleapis.com/css2?family=Kanit:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet"/>
         </Head>
         <body>
           <Main />
